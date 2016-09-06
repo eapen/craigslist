38533648c21954dad0608f2ab1b9491c2437cb95
Eapen <geapen@ebay.com>
2016-09-05 19:00:48 -0700

    Cleaning up

--- deployment/deploy.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-HOST=$1
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rsync -avz --exclude craigslist.db --exclude __pycache__ ../ $HOST:~/craigslist
-ssh $HOST '/bin/bash -s' < setup_scraper.sh
